c94d042b2e2757dbe1a8cb24f407f31535e35f19
Gabriel Cuello <cgabrielcuello@gmail.com>
2022-02-05 17:17:50 -0300

    5to commit: Generate Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { MoviesGrid } from "./MoviesGrid";
+import { MoviesGrid } from "./components/MoviesGrid";
 import styles from "./App.module.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 export function App() {
 	return (
-		<div>
+		<Router>
 			<header>
-				<h1 className={styles.title}>Movies</h1>
-            </header>
-            <main>
-               <MoviesGrid />
-            </main>
-		</div>
+				{/* <h1 className={styles.title}>Movies</h1> */}
+				<Link to="/">Home</Link>
+				<br />
+				<Link to="/movie">Movie</Link>
+			</header>
+			<main>
+				<Switch>
+					<Route exact path="/movie">
+						Movie
+					</Route>
+					<Route path="/">Home</Route>
+				</Switch>
+			</main>
+		</Router>
 	);
 }
