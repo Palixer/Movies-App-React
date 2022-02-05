a1493fd32fea0e88fcb9943fc10f708a3220a2fa
Gabriel Cuello <cgabrielcuello@gmail.com>
2022-02-05 12:12:25 -0300

    1er commit: Proyect sheet

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export function App() {
+	return (
+		<div>
+			<header>
+				<h1>Movies</h1>
+            </header>
+            <main>
+                <ul>
+                    <li></li>
+                </ul>
+            </main>
+		</div>
+	);
+}
